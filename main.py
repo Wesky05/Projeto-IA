85843cb3cfcbd2b732f95ac3fbfcb194e768dfb6
Wesky05 <kevinnyweslley@gmail.com>
2025-04-10 16:48:00 -0300

    htmls em rotas

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import pyodbc
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import PlainTextResponse
+from starlette.middleware.sessions import SessionMiddleware
+from fastapi.responses import RedirectResponse
+
+
 
 
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key="chave-secreta-bem-segura")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
+
 
 
 # Tela de login inicial
@@ -40,13 +48,23 @@ async def receber_dados(
     return templates.TemplateResponse(
         "formulário.html",{"request": request, "nome": nome})
 
-@app.post("/login-admin", response_class=HTMLResponse)
+@app.get("/dashboard", response_class=HTMLResponse)
+async def dashboard(request: Request):
+    usuario = request.session.get("usuario", "Desconhecido")
+    return templates.TemplateResponse("dashboard.html", {"request": request, "usuario": usuario})
+
+
+
+@app.post("/login-admin")
 async def login_admin(
     request: Request,
     usuario: str = Form(...),
     senha: str = Form(...)
 ):
-    return templates.TemplateResponse("admin_dashboard.html", {
-        "request": request,
-        "usuario": usuario
-    })
+
+    # Salvar o nome na sessão
+    request.session["usuario"] = usuario
+    # Validação pode entrar aqui
+    return RedirectResponse(url=f"/dashboard", status_code=302)
+
+    
